34217a3d327a771d773a6b0f7c21b617eefaeba4
Sem1hh <semihozturq@gmail.com>
2024-12-23 23:14:39 +0300

    Add files via upload

---
+++ WebProje/Models/ViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace BerberProject.Models
+{
+
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "User Password")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; } = false;
+    }
+    public class RegisterViewModel
+    {
+        [Required]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Name must contain only letters.")]
+        [MaxLength(16)]
+        [Display(Name = "Your name")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Name must contain only letters.")]
+        [MaxLength(16)]
+        [Display(Name = "Your surname")]
+        public string Surname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+    public class AdminDocViewModel
+    {
+        public Berber berber { get; set; }
+        public List<Berber> berber { get; set; }
+        public List<Department>? Departments { get; set; } = new List<Department>();
+
+    }
+    public class AdminDepViewModel
+    {
+        public Department department { get; set; }
+        public List<Department> Departments { get; set; }
+    }
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+
+    public class PatientViewModel :Appointment         // for new appointments
+=======
+    public class PatientViewModel        // for new appointments
+>>>>>>> Stashed changes
+=======
+    public class PatientViewModel        // for new appointments
+>>>>>>> 70bb60e2bba537100c2b20f3ee40149dd90b72a1
+    {
+        public Patient? Patient { get; set; }
+        public Appointment? Appointment { get; set; }
+        public List<Department>? Departments { get; set; } = new List<Department>();
+        public List<Berber>? berber { get; set; } = new List<berber>();
+    }
+}
