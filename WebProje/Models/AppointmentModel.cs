5d84025a1b3c344a789cec7b9b272987d9efd6fc
Eren Çoban <162314610+Surfinplus@users.noreply.github.com>
2024-12-24 23:41:35 +0300

    ts

---
+++ WebProje/Models/AppointmentModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarberProject.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int? Id { get; set; }
+        public DateTime Date { get; set; }
+        public Barber? Barber { get; set; }
+        public int? BarberId {  get; set; }   // foreign key
+        public Department? Department { get; set; }
+        public int? DepartmentId { get; set; }       // foreign key
+        public Customer? Customer { get; set; }
+        public string CustomerId { get; set; }   // foreign key
+
+    }
+}
