b121ba6d323f981d58081b0fd97c259aaaa21277
Sem1hh <semihozturq@gmail.com>
2024-12-24 23:40:41 +0300

    islem

--- WebProje/Models/Kategori.cs
+++ WebProje/Models/Kategori.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace HospitalProject.Models
+namespace BerberProject.Models
 {
     public class Department
     {
@@ -9,7 +9,7 @@ namespace HospitalProject.Models
         [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "Department name must contain only letters.")]
         public string DepartmentName { get; set; }
         public string PhoneNumber { get; set; }
-        public ICollection<Doctor>? Doctors { get; set; } = new List<Doctor>();
+        public ICollection<berber>? Berbers { get; set; } = new List<Berber>();
         public ICollection<Appointment>? Appointments { get; set; } = new List<Appointment>();
     }
 }
